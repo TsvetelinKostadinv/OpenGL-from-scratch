dbb91880a64fb72e4b6f0a63e0c78e24ca5b51af
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2021-10-07 19:52:30 +0300

    Added more template arguments and added SFINAE enable/disable of constructors based on properties of the type

--- OpenGL-from-scratch/src/util/strong_type.h
+++ OpenGL-from-scratch/src/util/strong_type.h
@@ -1,33 +1,44 @@
 #pragma once
+#include <ratio>
 #include <type_traits>
+
 #include "typedef.h"
 
-template <typename T>
+template <typename T, typename Identifier, typename... Decorators>
 struct strong_type
 {
+    template <typename = std::enable_if_t<std::is_default_constructible_v<T>>>
     constexpr explicit strong_type() noexcept(
-        std::is_trivially_constructible_v<T>)
+        std::is_nothrow_default_constructible_v<T>)
         : value()
     {
     }
 
+    template <typename = std::enable_if_t<std::is_copy_constructible_v<T>>>
     constexpr explicit strong_type(const T& value) noexcept(
         std::is_nothrow_copy_constructible_v<T>)
-        : value(value){};
+        : value(value)
+    {
+    }
 
-    constexpr explicit strong_type(T&& value) noexcept(
-        std::is_nothrow_move_constructible_v<T>)
-        : value(std::move(value)){};
-
+    template <typename = std::enable_if_t<std::is_copy_constructible_v<T>>>
     constexpr explicit strong_type(const strong_type& other) noexcept(
         std::is_nothrow_copy_constructible_v<T>)
         : value(other.value)
     {
     }
 
+    template <typename = std::enable_if_t<std::is_move_constructible_v<T>>>
+    constexpr explicit strong_type(T&& value) noexcept(
+        std::is_nothrow_move_constructible_v<T>)
+        : value(std::forward(value))
+    {
+    }
+
+    template <typename = std::enable_if_t<std::is_move_constructible_v<T>>>
     constexpr explicit strong_type(strong_type&& other) noexcept(
         std::is_nothrow_move_constructible_v<T>)
-        : value(std::move(other.value))
+        : value(std::forward(other.value))
     {
     }
 
@@ -39,3 +50,6 @@ struct strong_type
     // contained on the stack like a usual variable
     T value;
 };
+
+template <typename T, typename Identifier, typename... Decorators>
+using primitive_type = strong_type<T, Identifier, Decorators...>;
